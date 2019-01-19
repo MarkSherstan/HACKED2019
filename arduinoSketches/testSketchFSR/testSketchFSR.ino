fb732c27e5d6c4cf5199593e06b66f7779c541ae
Eric <ewells@ualberta.ca>
2019-01-19 16:52:21 -0700

    Multiple Input Channels

--- arduinoSketches/testSketchFSR/testSketchFSR.ino
+++ arduinoSketches/testSketchFSR/testSketchFSR.ino
@@ -1,9 +1,11 @@
 // Declare some variables
-const int FSR_PIN = A0;
+const int FSR_PIN[] = {0, 1, 2, 3, 4, 5, 6, 7};
+
 const float VCC = 4.98;
 const float R_DIV = 5100.0;
+ 
 int fsrADC;
-float force;
+float force[8];
 float fsrV;
 float fsrR;
 float fsrG;
@@ -12,30 +14,41 @@ float fsrG;
 void setup(){
   // Set up serial and pin mode
   Serial.begin(9600);
-  pinMode(FSR_PIN, INPUT);
+  for (int i=0;i<8;i++){
+  pinMode(FSR_PIN[i], INPUT);
+  }
 }
 
 
 void loop(){
-  // Read pin
-  fsrADC = analogRead(FSR_PIN);
 
-  // Use ADC reading to calculate voltage:
-  fsrV = fsrADC * VCC / 1023.0;
+  for (int i=0; i<8;i++){
+      // Read pin
+    fsrADC = analogRead(FSR_PIN[i]);
+  
+    // Use ADC reading to calculate voltage:
+    fsrV = fsrADC * VCC / 1023.0;
+  
+    // Use voltage and static resistor value to calculate FSR resistance:
+    fsrR = ((VCC - fsrV) * R_DIV) / fsrV;
+  
+    // Guesstimate force based on slopes in figure 3 of FSR datasheet (conductance):
+    fsrG = 1.0 / fsrR;
+  
+    // Break parabolic curve down into two linear slopes:
+    if (fsrR <= 600)
+      force[i] = (fsrG - 0.00075) / 0.00000032639;
+    else
+      force[i] =  fsrG / 0.000000642857;
+    
+  }
 
-  // Use voltage and static resistor value to calculate FSR resistance:
-  fsrR = ((VCC - fsrV) * R_DIV) / fsrV;
-
-  // Guesstimate force based on slopes in figure 3 of FSR datasheet (conductance):
-  fsrG = 1.0 / fsrR;
-
-  // Break parabolic curve down into two linear slopes:
-  if (fsrR <= 600)
-    force = (fsrG - 0.00075) / 0.00000032639;
-  else
-    force =  fsrG / 0.000000642857;
 
   // Display data [in grams] and add small pause
-  Serial.println(force);
-  delay(1);
+    for (int i=0; i<8; i++){
+        Serial.print(force[i]);
+        Serial.print(',');
+    }
+    Serial.println();
+    delay(1);
 }
